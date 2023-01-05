fa107469f55877c3de58f2ede453b5192f19b32c
Conor Collier <S00224844@atu.ie>
2023-01-05 10:16:48 +0000

    q1(e) added team class

---
+++ JanExam/Team.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JanExam
+{
+    internal class Team
+    {
+        //properties
+        private string Name { get; set; }
+        private List<Player> Players { get; set; }
+
+        //constructors
+        public Team(string name, List<Player> players)
+        {
+            Name = name;
+            Players = players;
+        }
+    }
+}
