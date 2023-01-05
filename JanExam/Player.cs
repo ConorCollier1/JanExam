87e9e99b7ae97daac2ad5bbbfe117f9fa44563da
Conor Collier <S00224844@atu.ie>
2023-01-05 10:14:18 +0000

    q1(d) added Player class

---
+++ JanExam/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JanExam
+{
+    internal class Player
+    {
+        //properties
+        private string Name { get; set; }
+        private string ResultRecord { get; set; }
+
+        //constructors
+        public Player(string name, string resultRecord)
+        {
+            Name = name;
+            ResultRecord= resultRecord;
+        }
+    }
+}
